2aef89d77f943cecd32b1784e18127ec05ce8a59
Kien Nguyen <kc97ble@gmail.com>
2021-04-18 12:40:21 +0800

    Initialize app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # email-service
+
+## Setup
+
+Run `npm install`
+
+## Start development server
+
+Run `npm run build-and-watch` in one console window.
+
+Run `npm run serve` in another console window.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+import v1 from "./v1";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("json spaces", 2);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    service_name: "email_service",
+  });
+});
+
+app.use("/email/v1", v1);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
