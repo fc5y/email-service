45fcdf7d9b4e3692aeddfa7003fc088b8b122e5c
sonnguyen112 <80495667+sonnguyen112@users.noreply.github.com>
2021-09-23 07:44:06 +0700

    fix

--- src/v1/logic/templates.ts
+++ src/v1/logic/templates.ts
@@ -4,32 +4,32 @@ type Template = {
 };
 
 const TEMPLATES: Record<number, Template> = {
-    10001: {
-      subject: "{{otp}} là mã xác minh Free Contest của bạn.",
-      content:
-        "" +
-        "Chào bạn {{displayed_name}}, \n" +
-        "\n" +
-        "Bạn đang tạo một tài khoản Free Contest. \n" +
-        "Mã xác minh của bạn là: {{otp}}. \n" +
-        "Mã này có hiệu lực trong 10 phút. \n" +
-        "\n" +
-        "Thân mến, \n" +
-        "FreeContest.",
-    },
-    10002: {
-      subject: "{{otp}} là mã xác minh Free Contest của bạn.",
-      content:
+  10001: {
+    subject: "{{otp}} là mã xác minh Free Contest của bạn.",
+    content:
       "" +
-        "Chào bạn {{displayed_name}}, \n" +
-        "\n" +
-        "Bạn vừa yêu cầu đặt lại mật khẩu cho tài khoản {{username}}. \n" +
-        "Mã xác minh của bạn là: {{otp}}. \n" +
-        "Mã này có hiệu lực trong 10 phút. \n" +
-        "\n" +
-        "Thân mến, \n" +
-        "FreeContest.",
-    }
+      "Chào bạn {{displayed_name}}, \n" +
+      "\n" +
+      "Bạn đang tạo một tài khoản Free Contest. \n" +
+      "Mã xác minh của bạn là: {{otp}}. \n" +
+      "Mã này có hiệu lực trong 10 phút. \n" +
+      "\n" +
+      "Thân mến, \n" +
+      "FreeContest.",
+  },
+  10002: {
+    subject: "{{otp}} là mã xác minh Free Contest của bạn.",
+    content:
+      "" +
+      "Chào bạn {{displayed_name}}, \n" +
+      "\n" +
+      "Bạn vừa yêu cầu đặt lại mật khẩu cho tài khoản {{username}}. \n" +
+      "Mã xác minh của bạn là: {{otp}}. \n" +
+      "Mã này có hiệu lực trong 10 phút. \n" +
+      "\n" +
+      "Thân mến, \n" +
+      "FreeContest.",
+  }
 };
 
 function fillString(currentString: string, params: { [key: string]: string }) {
